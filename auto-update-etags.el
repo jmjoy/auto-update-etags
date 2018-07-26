ec9d4ea49de0b805c6b4e1e731464f657ebf923e
__JM_Joy__ <918734043@qq.com>
2018-07-26 18:44:56 +0800

    First

--- auto-update-etags.el
+++ auto-update-etags.el
@@ -1,24 +1,36 @@
-(defvar-local update-async-lock (make-mutex "update-async-lock"))
+(require 'etags)
 
-(defun auto-update-etags--update-file (file)
-  (message file))
+(defvar auto-update-etags/update-async-lock (make-mutex "auto-update-etags/update-async-lock"))
 
-(setq file-name "hehhe")
+(defvar auto-update-etags/update-buffers-queue nil)
 
-(defun auto-update-etags--update-async ()
-  (let ((current-file-name (buffer-file-name)))
-    (when current-file-name
-      (make-thread (lambda ()
-                       (message (format "HEHE2: %s" file-name))
-                       ;; (with-mutex update-async-lock
-                       ;;   (auto-update-etags--update-file current-file-name))
-                       )))))
+(defun auto-update-etags/consume-update-buffers ()
+  (when-let ((buffer (pop auto-update-etags/update-buffers-queue)))
+    (with-current-buffer buffer
+      (let ((-buffer-file-name (buffer-file-name))
+            (-tags-file-name tags-file-name))
+        (setq -buffer-file-name "/home/jmjoy/works/world/v3/include/easy/easy_order_class.inc.php")
+        (setq -tags-file-name "/home/jmjoy/works/TAGS")
+        (when (and -buffer-file-name -tags-file-name)
+          (let ((tags-file-directory (file-name-directory -tags-file-name)))
+            (when (string-prefix-p tags-file-directory -tags-file-name)
+              (let ((file-relative-name -buffer-file-name tags-file-directory))
+                ))))))))
+
+(defun auto-update-etags/update-buffer ()
+  (when (buffer-file-name)
+    (push (current-buffer) auto-update-etags/update-buffers-queue)
+    (make-thread (lambda ()
+                   (with-demoted-errors "Auto update etags error: %s"
+                     (with-mutex auto-update-etags/update-async-lock
+                       (auto-update-etags/consume-update-buffers)))))))
 
 (define-minor-mode auto-update-etags-mode
   "Update etags TAGS file on save."
+  :variable nil
   (if auto-update-etags-mode
-      (add-hook 'after-save-hook 'auto-update-etags--update-async nil 'local)
-    (remove-hook 'after-save-hook 'auto-update-etags--update-async 'local))
+      (add-hook 'after-save-hook 'auto-update-etags/update-buffer nil 'local)
+    (remove-hook 'after-save-hook 'auto-update-etags/update-buffer 'local))
   (setq tags-revert-without-query t))
 
 (provide 'auto-update-etags)
